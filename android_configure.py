c3c897bd6e85e600377719ebf7f485a4a576f95e
FoxNick <354435383@qq.com>
2025-01-22 03:25:53 +0800

    Update android_configure.py

--- android_configure.py
+++ android_configure.py
@@ -82,4 +82,4 @@ os.environ['GYP_DEFINES'] = GYP_DEFINES
 
 if os.path.exists("./configure"):
     # nodejs-mobile patch: added --with-intl=none and --shared
-    os.system("./configure --dest-cpu=" + DEST_CPU + " --dest-os=android --openssl-no-asm --cross-compiling --enable-static ")
+    os.system("./configure --dest-cpu=" + DEST_CPU + " --dest-os=android --openssl-no-asm --cross-compiling --v8-lite-mode=true --shared ")
